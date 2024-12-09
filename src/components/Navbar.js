331b831696dc38d9de8d3b8e644234d5d422aebb
Guillaume Sere <guillaumesere@hotmail.fr>
2024-12-09 16:26:04 +0100

    add icon to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../logo.svg'
+import homeIcon from '../images/icon.png'
+import infoIcon from '../images/info.png'  
 export default function Navbar() {
   return (
     <nav className='navbar'>
@@ -10,10 +12,14 @@ export default function Navbar() {
         </Link>
         <ul className='nav-links'>
           <li>
-            <Link to='/'>home</Link>
+            <Link to='/'>
+              <img src={homeIcon} alt='icon home' className='home-icon' />
+            </Link>
           </li>
           <li>
-            <Link to='/about'>Info</Link>
+            <Link to='/about'>
+            <img src={infoIcon} alt='icon info' className='info-icon' />
+            </Link>
           </li>
         </ul>
       </div>
