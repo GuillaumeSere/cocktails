4d9b41ea80a5df76a4c2482505452aa0ac1d3df9
Guillaume Sere <guillaumesere@hotmail.fr>
2024-12-09 12:25:57 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">à propos de nous</h1>
+            <p>
+                Bienvenue sur notre site de recherche de cocktails !
+                Nous sommes ravis de vous proposer une plateforme
+                unique pour découvrir et explorer les délices du
+                monde des cocktails. Grâce à l'API de TheCocktailDB,
+                nous vous offrons accès à une vaste collection de
+                recettes de cocktails, ainsi qu'à des informations
+                détaillées sur chaque boisson. Que vous soyez un
+                amateur de cocktails ou un professionnel de l'industrie,
+                notre site est conçu pour vous aider à trouver l'inspiration
+                pour votre prochaine création ou simplement pour
+                vous aider à choisir le cocktail parfait pour votre soirée.
+            </p>
+        </section>
+    );
+}
