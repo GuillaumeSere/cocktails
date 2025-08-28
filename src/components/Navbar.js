487764f913c161f164285fc6f6a9ce2571e61028
Guillaume Sere <guillaumesere@hotmail.fr>
2025-08-28 14:39:18 +0200

    add faq

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../logo.svg'
 import homeIcon from '../images/icon.png'
-import infoIcon from '../images/info.png'  
-export default function Navbar() {
+import infoIcon from '../images/info.png'
+
+const Navbar = () => {
+  const [activeTooltip, setActiveTooltip] = useState(null)
+
+  const handleMouseEnter = (tooltipId) => {
+    setActiveTooltip(tooltipId)
+  }
+
+  const handleMouseLeave = () => {
+    setActiveTooltip(null)
+  }
+
   return (
     <nav className='navbar'>
       <div className='nav-center'>
@@ -11,18 +22,38 @@ export default function Navbar() {
           <img src={logo} alt='cocktail db logo' className='logo' />
         </Link>
         <ul className='nav-links'>
-          <li>
-            <Link to='/'>
+          <li 
+            className='nav-item'
+            onMouseEnter={() => handleMouseEnter('home')}
+            onMouseLeave={handleMouseLeave}
+          >
+            <Link to='/' aria-label='Accueil'>
               <img src={homeIcon} alt='icon home' className='home-icon' />
             </Link>
+            {activeTooltip === 'home' && (
+              <div className='tooltip' role='tooltip' aria-hidden='false'>
+                Accueil
+              </div>
+            )}
           </li>
-          <li>
-            <Link to='/about'>
-            <img src={infoIcon} alt='icon info' className='info-icon' />
+          <li 
+            className='nav-item'
+            onMouseEnter={() => handleMouseEnter('about')}
+            onMouseLeave={handleMouseLeave}
+          >
+            <Link to='/about' aria-label='À propos'>
+              <img src={infoIcon} alt='icon info' className='info-icon' />
             </Link>
+            {activeTooltip === 'about' && (
+              <div className='tooltip' role='tooltip' aria-hidden='false'>
+                À propos
+              </div>
+            )}
           </li>
         </ul>
       </div>
     </nav>
   )
 }
+
+export default Navbar
--- src/pages/About.js
+++ src/pages/About.js
@@ -17,6 +17,32 @@ export default function About() {
                 pour votre prochaine création ou simplement pour
                 vous aider à choisir le cocktail parfait pour votre soirée.
             </p>
+            <h2 className="section-title">FAQ</h2>
+            <p>
+                <span>1. Quels sont les cocktails les plus populaires ?</span><br></br>
+                Les plus connus et appréciés restent le Mojito, la Margarita, le Cosmopolitan,
+                le Daiquiri, le Caipirinha ou encore le Bloody Mary.<br></br>
+                <span>2. Peut-on préparer des cocktails sans alcool ?</span><br></br>
+                Oui, bien sûr ! Les cocktails sans alcool (aussi appelés mocktails) sont
+                une excellente alternative. Tu peux utiliser des jus de fruits, du sirop,
+                de l’eau gazeuse et des herbes aromatiques pour obtenir des boissons fraîches et savoureuses.
+                <br></br>
+                <span>3. De quoi ai-je besoin pour commencer à faire des cocktails ?</span><br></br>
+                Les indispensables sont : un shaker, une cuillère à mélange, un doseur,
+                et quelques verres adaptés. Ensuite, il te suffit d’avoir quelques alcools
+                de base (rhum, vodka, gin, tequila) et des jus de fruits pour varier les recettes.
+                <br></br>
+                <span>4. Comment trouver une recette selon les ingrédients que j’ai à la maison ?</span><br></br>
+                Sur notre site, Cocktails, tu peux rechercher un cocktail par ingrédient
+                 : entre le nom de ce que tu as (par exemple "rhum" ou "vodka") 
+                 et découvre toutes les recettes que tu peux préparer facilement.
+                 <br></br>
+                 <span>5. Quelle est la différence entre un cocktail et un long drink ?</span><br></br>
+                 Un cocktail est une boisson généralement servie dans un petit verre
+                  (souvent plus concentrée en alcool), tandis qu’un long drink est un 
+                  cocktail allongé avec un liquide non alcoolisé (soda, jus, eau gazeuse),
+                   ce qui donne une boisson plus grande et légère.
+            </p>
         </section>
     );
 }
